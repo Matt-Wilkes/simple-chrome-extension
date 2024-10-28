963839cba9513262a6f302fa84ff16bbebde9c3b
Matt Wilkes <matthew.wilkes90@gmail.com>
2024-10-28 20:33:45 +0000

    build: colour changer * add function to change colour of window background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+// this is being stored inside the popup
+  const [colour, setColour] = useState("");
+
+  const onClick = async () => {
+    // this is happening inside the page
+    let [tab] = await chrome.tabs.query({ active: true });
+    chrome.scripting.executeScript<string[], void>({
+      target: { tabId: tab.id!},
+      // pass the colour variable into the args of the script
+      args: [colour],
+      // then pass it as an argument to the function
+      func: (colour) => {
+        document.body.style.backgroundColor = colour
+      }
+    });
+  }
 
   return (
     <>
@@ -16,11 +31,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>My Extension</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => onClick()}>
+          Click me!
         </button>
+        <input type="color" onChange={(e) => setColour(e.currentTarget.value)}></input>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
