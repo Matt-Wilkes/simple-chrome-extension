256e566aad5da6cabb5a740dd020e844a7c00bbb
Matt Wilkes <matthew.wilkes90@gmail.com>
2024-11-07 17:42:06 +0000

    feat/ add a saved tab with keyboard shortcut * abstract function to morph the tab data

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,32 @@
 // Allows users to open the side panel by clicking on the action toolbar icon
-chrome.sidePanel
+
+  chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
   .catch((error) => console.error(error));
-
+  
 
 chrome.commands.onCommand.addListener((command) => {
   if (command === "save_tab") {
     console.log(`Save tab triggered`);
+    sendTabToApp();
   } else if (command === "open_sidebar") {
     console.log(`open sidebar triggered`);
+    // togglePanel();
+    chrome.sidePanel.open
+    
   }
 });
+
+const sendTabToApp = async () => {
+  let [activeTab] = await chrome.tabs.query({ active: true });
+      // const tab = {
+      //   url: activeTab.url,
+      //   title: activeTab.title,
+      //   favIconUrl: activeTab.favIconUrl,
+      // };
+      console.log('tab = ', activeTab)
+      // Send the tab info to the React app
+      chrome.runtime.sendMessage({ message: "Tab Info", data: activeTab });
+
+}
 
--- src/App.tsx
+++ src/App.tsx
@@ -7,45 +7,62 @@ import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 
 function App() {
-// this is being stored inside the popup
-// const [savedTab, setSavedTab] = useState<SavedUrl>({
-//   url: "https://reactnative.dev/docs/turbo-native-modules-introduction",
-//   parsedUrl: "reactnative.dev",
-//   description: "Learn react native",
-//   favicon: "https://reactnative.dev/img/favicon.ico"
-// });
- const [savedLinks, setSavedLinks] = useState<SavedUrl[]>([])
+  // this is being stored inside the popup
+  const [savedLinks, setSavedLinks] = useState<SavedUrl[]>([])
 
- function shortenUrl(url:string): string {
-  try {
-    const parsedUrl = new URL(url);
-    return parsedUrl.host;
-    
-  } catch (error) {
-    console.error("Invalid Url", error);
-    return url;
+    // React to changes in the savedLinks array
+    useEffect(() => {
+      console.log('saved links', savedLinks)
+    }, [savedLinks])
+  
+    useEffect(() => {
+      //listener to receive messages from the service worker
+      const handleMessage = (receivedMessage: { message: string; data: chrome.tabs.Tab }) => {
+        if (receivedMessage?.message === "Tab Info") {
+          console.log("Received tab info:", receivedMessage.data);
+          updateSavedLinks(modifyTabData(receivedMessage.data));
+        }
+      };
+  
+      // Attach the listener for runtime messages
+      chrome.runtime.onMessage.addListener(handleMessage);
+  
+      // Clean up the listener on component unmount
+      // return () => chrome.runtime.onMessage.removeListener(handleMessage);
+    }, []);
+
+  function shortenUrl(url: string): string {
+    try {
+      const parsedUrl = new URL(url);
+      return parsedUrl.host;
+
+    } catch (error) {
+      console.error("Invalid Url", error);
+      return url;
+    }
   }
- }
 
+  async function handleClick() {
+    let [activeTab] = await chrome.tabs.query({ active: true });
+    updateSavedLinks(modifyTabData(activeTab))
+  }
 
-  useEffect(() => {
-    // console.log('saved tab', savedTab)
-    console.log('saved links', savedLinks)
-  }, [savedLinks])
+  const modifyTabData = (tabData: chrome.tabs.Tab) => {
 
-  const onClick = async () => {
-    // this is happening inside the page
-    let [tab] = await chrome.tabs.query({ active: true });
-    let parsed = shortenUrl(`${tab.url}`);
+    let parsed = shortenUrl(`${tabData.url}`);
 
     const newTab = {
-      url: `${tab.url}`,
+      url: `${tabData.url}`,
       parsedUrl: parsed,
-      description: `${tab.title}`,
-      favicon: `${tab.favIconUrl}`
+      description: `${tabData.title}`,
+      favicon: `${tabData.favIconUrl}`
     }
-    
-    setSavedLinks(prevSavedLinks => [...prevSavedLinks, newTab])
+
+    return newTab
+  }
+
+  const updateSavedLinks = async (modifiedTab: SavedUrl) => {
+    setSavedLinks(prevSavedLinks => [...prevSavedLinks, modifiedTab])
   }
 
 
@@ -53,25 +70,25 @@ function App() {
     <>
       <h1>Up Next</h1>
       <div className="card">
-        <button onClick={() => onClick()}>
+        <button onClick={() => handleClick()}>
           Add to up next
         </button>
       </div>
       <Box sx={{ flexGrow: 1, maxWidth: '100%' }}>
-      {/* <Grid container spacing={5}> */}
+        {/* <Grid container spacing={5}> */}
         {/* <Grid size={{xs:12, md:6}}> */}
-            <List 
-            >
-              { savedLinks.map((item: SavedUrl) => {
-                return (
-                  <SavedTab link={item}/>
-              );
-            })
-            }
-            </List>
+        <List
+        >
+          {savedLinks.map((item: SavedUrl) => {
+            return (
+              <SavedTab link={item} />
+            );
+          })
+          }
+        </List>
         {/* </Grid> */}
-      {/* </Grid> */}
-    </Box>
+        {/* </Grid> */}
+      </Box>
     </>
   )
 }
