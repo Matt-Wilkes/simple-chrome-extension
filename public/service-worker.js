9e0e4007cad215f990c9bc799ba6a7f212d6178a
Matt Wilkes <matthew.wilkes90@gmail.com>
2024-11-19 21:56:39 +0000

    feat/ update tabs with keyboard shortcut: * add functionality to save tabs with sidebar closed using keyboard shortcut * update manifest permissions to use local storage * seperation of concerns for functionality on homepage * update service-worker.js to use local storage

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,28 +1,44 @@
-// Allows users to open the side panel by clicking on the action toolbar icon
-
-  chrome.sidePanel
+chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
   .catch((error) => console.error(error));
-  
 
-chrome.commands.onCommand.addListener((command) => {
-  if (command === "save_tab") {
-    console.log(`Save tab triggered`);
-    sendTabToApp();
-  } else if (command === "open_sidebar") {
-    console.log(`open sidebar triggered`);
-    // togglePanel();
-    chrome.sidePanel.getOptions
-    
-  }
+//developer.chrome.com/docs/extensions/reference/api/sidePanel
+https: chrome.commands.onCommand.addListener((command, tab) => {
+  (async () => {
+    if (command === "save_tab") {
+      console.log(`Save tab triggered`);
+      // const tabData = await getCurrentTab();
+      sendTabToApp()
+    } else if (command === "open_sidebar") {
+      console.log(`open sidebar triggered`);
+      await chrome.sidePanel.open({ windowId: tab.windowId });
+    }
+  })();
 });
 
-const sendTabToApp = async () => {
-  let [activeTab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-      console.log('tab = ', activeTab)
-      // Send the tab info to the React app
-      chrome.runtime.sendMessage({ message: "Tab Info", data: activeTab });
-
+async function sendTabToApp() {
+  try {
+    const activeTab = await getCurrentTab();
+    chrome.storage.local.get("tabsData", (result) => {
+      const tabsData = result.tabsData || [];
+      console.log("Existing tabs data:", tabsData);
+      tabsData.push(activeTab);
+      chrome.storage.local.set({ tabsData }, () => {
+        console.log("Tab data added and saved:", activeTab);
+      });
+    });
+    // Send a message with tab data
+    // await chrome.runtime.sendMessage({ message: "tab_data", data: activeTab });
+  } catch (error) {
+    console.error("Error storing tab:", error);
+  }
 }
 
+async function getCurrentTab() {
+  const [activeTab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true,
+  });
+  console.log(activeTab);
+  return activeTab;
+}
